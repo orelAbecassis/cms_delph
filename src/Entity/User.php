a0aa91baf551030b48889490134c24cc3a1f1244
orelabecassis <orelabk6.oa.oa@gmail.com>
2023-04-25 12:06:04 +0200

    Fix user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -76,14 +76,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_COMPTABLE';
-        $roles[] = 'ROLE_ADMIN';
-        $roles[] = 'ROLE_CLIENT ';
+        $roles[] = 'ROLE_USER';
 
-        return array_unique($roles);
+        return array_values(array_unique($roles));
     }
-
     public function setRoles(array $roles): self
     {
         $this->roles = $roles;
@@ -173,5 +169,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         }
 
         return $this;
+    }
+
+    public function __toString() {
+        return$this->getEmail();
     }
 }
