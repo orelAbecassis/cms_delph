66e781ddf6a61b1c267566f5494557259eac1d83
root <Nathan55555>
2023-04-30 20:20:40 +0200

    Version Nathan

--- src/Controller/FichierDemandeController.php
+++ src/Controller/FichierDemandeController.php
@@ -5,13 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\InfoClientRepository;
 
 class FichierDemandeController extends AbstractController
 {
     #[Route('/fichier/demande', name: 'app_fichier_demande')]
-    public function index(): Response
+    public function index(InfoClientRepository $infoClientRepository): Response
     {
+        $user = $this->getUser();
         return $this->render('fichier_demande/index.html.twig', [
+            'info_clients' => $infoClientRepository->findAll(),
+            'user'=>$user->getUserIdentifier(),
             'controller_name' => 'FichierDemandeController',
         ]);
     }
